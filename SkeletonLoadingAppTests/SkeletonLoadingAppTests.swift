bbe4d24e7fc1507e3ea1ff18fe186c566fc70033
saadetsimsek <saadetsimsek13@gmail.com>
2024-10-29 15:44:33 +0300

    Initial Commit

---
+++ SkeletonLoadingAppTests/SkeletonLoadingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkeletonLoadingAppTests.swift
+//  SkeletonLoadingAppTests
+//
+//  Created by Saadet Şimşek on 29/10/2024.
+//
+
+import Testing
+@testable import SkeletonLoadingApp
+
+struct SkeletonLoadingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
